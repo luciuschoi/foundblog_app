b616a60215ac9fbda1dc6f1fc8568a2e20501663
Lucius Choi <lucius.choi@gmail.com>
2016-05-18 16:19:08 +0900

    제04장 : 다이나믹 레이아웃의 작성

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    # Foundation 6 : Color symbol
+    # 'primary', 'secondary', 'success', 'alert', and 'warning'
+    case flash_type
+      when "success"
+        "success"       # Green
+      when "error"
+        "warning"       # Yellow
+      when "alert"
+        "alert"         # Red
+      when "notice"
+        "primary"       # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
+  def active_menu(target_controller)
+    "class=#{controller_name == target_controller ? 'active' : ''}"
+  end
+
+  def user_email(post)
+    post.user.present? ? post.user.email : "an anonymous user"
+  end
+
+  def user_roles(user)
+    user.roles.map(&:name).join(', ').titleize
+  end
 end
